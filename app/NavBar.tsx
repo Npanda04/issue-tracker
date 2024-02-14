a3a4e6a99eb87b1a977e6e5462c257b88e997ec0
Npanda04 <deepanshusisodiya21@gmail.com>
2024-02-14 19:08:12 +0530

    styled navbar and link's

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,21 @@
+'use client';
+
+
 import React from 'react'
 import Link from "next/link"
+import { usePathname } from 'next/navigation';
+
+import classnames from "classnames"
+
+
 import { IoIosBug } from "react-icons/io";
 
+
+
+
 const NavBar = () => {
+    const currentPath = usePathname()
+    console.log(currentPath)
 
     const links = [
         { label: "Dashboard", href: "/" },
@@ -10,12 +23,16 @@ const NavBar = () => {
     ]
     return (
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center' >
-            <Link href="/"><IoIosBug /></Link>
+            <Link href="/"><IoIosBug color="black"/></Link>
             <ul className=' flex space-x-6'>
                 {links.map((link) => (
                     <Link
                         key={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className={classnames({
+                            'text-zinc-900': link.href === currentPath,
+                            'text-zinc-500': link.href != currentPath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })}
                         href={link.href}>
                         {link.label}
                     </Link>
